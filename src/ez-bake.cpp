c31119286b4555ea638e2455cea2f862e0844587
Sam Austin <austinsp@mit.edu>
2023-02-18 21:45:28 -0500

    Converted temp array to type std::vector from C array

--- src/ez-bake.cpp
+++ src/ez-bake.cpp
@@ -14,37 +14,39 @@ bool lastHeaterState = false;
 bool targetHeaterState = false;
 float tmp = 0;
 
-
 void setup(void) {
   Serial.begin(SERIAL_BAUDRATE);
   sensors.begin();
   pinMode(OUTPUT_PIN, OUTPUT);
   // Write last heater state to the pin on startup so it is always correct
   digitalWrite(OUTPUT_PIN, lastHeaterState);
+
+  const uint8_t NUM_THERMOCOUPLES = sensors.getDeviceCount();
+  temperatures.resize(NUM_THERMOCOUPLES, 0);
 }
 
-bool areAllTemperaturesValid(tempCArray temperatures) {
-  for (int8_t i = 0; i < NUM_THERMOCOUPLES; i++) {
-    float tempC = temperatures[i];
-    if (tempC == DEVICE_DISCONNECTED ||
-        tempC < MIN_LEGAL_TEMP_C ||
-        tempC > MAX_LEGAL_TEMP_C) {
+bool areAllTemperaturesValid() {
+  for (auto& reading: temperatures) {
+    if (reading == DEVICE_DISCONNECTED ||
+        reading < MIN_LEGAL_TEMP_C ||
+        reading > MAX_LEGAL_TEMP_C) {
       return false;
     }
   }
   return true;
 }
 
-void readThermocouples(tempCArray temperatures) {
+void readThermocouples() {
+  const uint8_t NUM_THERMOCOUPLES = temperatures.size();
   sensors.requestTemperatures();
   for (int8_t i = 0; i < NUM_THERMOCOUPLES; i++ ) {
     temperatures[i] = sensors.getTempCByIndex(i);
   }
 }
 
-void serialPrintSummary(tempCArray temperatures, bool targetHeaterState, float currentSetpointC, int isUnsafe) {
-  for (int i = 0; i < NUM_THERMOCOUPLES; i++ ) {
-      Serial.print(temperatures[i]);
+void serialPrintSummary(bool targetHeaterState, float currentSetpointC, int isUnsafe) {
+  for (auto& reading: temperatures) {
+      Serial.print(reading);
       Serial.print(" ");
   }
   Serial.print(targetHeaterState);
@@ -55,18 +57,17 @@ void serialPrintSummary(tempCArray temperatures, bool targetHeaterState, float c
   Serial.println();
 }
 
-bool getHeaterControlState(tempCArray temperatures, float setpointC) {
+bool getHeaterControlState(float setpointC) {
   // Get the mean temperature
   float meanTemperatureC = 0;
   float validTempCounts = 0;
-  for (int i = 0; i < NUM_THERMOCOUPLES; i++ ) {
-      if (!isnan(temperatures[i])){
-        meanTemperatureC += temperatures[i];
+  for (auto& reading: temperatures) {
+      if (!isnan(reading)){
+        meanTemperatureC += reading;
         validTempCounts++;
       }
   }
   meanTemperatureC /= validTempCounts;
-  // Serial.println(meanTemperatureC);
 
   // Simple bang-bang control
   return meanTemperatureC < setpointC;
@@ -89,15 +90,15 @@ void loop(void) {
       }
     }
 
-  readThermocouples(temperatures);   // Reads all thermocouples
+  readThermocouples();   // Reads all thermocouples
   
-  if (!areAllTemperaturesValid(temperatures)) {
+  if (!areAllTemperaturesValid()) {
     isUnsafe = 2;   // isUnsafe == 2 means that the controller detected a fault condition
     currentSetpointC = 0;
   }
 
   if (!isUnsafe){
-    targetHeaterState = getHeaterControlState(temperatures, currentSetpointC);
+    targetHeaterState = getHeaterControlState(currentSetpointC);
     loopsSinceStateChange = min(loopsSinceStateChange + 1, DEBOUNCE_LOOPS);
     if (targetHeaterState == lastHeaterState || loopsSinceStateChange < DEBOUNCE_LOOPS) {
       targetHeaterState = lastHeaterState; // Either we don't want to change our control state, or aren't allowed to yet. Updated so that the heater state can be printed out along with the sensor summaries on every loop
@@ -106,7 +107,7 @@ void loop(void) {
   else
     targetHeaterState = 0;
 
-  serialPrintSummary(temperatures, targetHeaterState, currentSetpointC, isUnsafe);    // Moving this line down so the heater status can be printed after the bang-bang calculation
+  serialPrintSummary(targetHeaterState, currentSetpointC, isUnsafe);    // Moving this line down so the heater status can be printed after the bang-bang calculation
 
   // Adding a conditional so that the heater state can be printed out at every iteration - avoids breaking out of loop() if the state is not updated
   if ((targetHeaterState != lastHeaterState) && !isUnsafe){
